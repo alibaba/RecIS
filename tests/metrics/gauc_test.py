7a66ced823df6b4b453baabe922bd5d1bd9a4168
xnuohz <xnuohz@126.com>
2026-01-15 21:04:33 +0800

    update

--- tests/metrics/gauc_test.py
+++ tests/metrics/gauc_test.py
@@ -7,7 +7,7 @@ from recis.metrics.gauc import Gauc
 
 class TestGauc(unittest.TestCase):
     def test_gauc(self):
-        labels = torch.tensor([1, 0, 0, 0, 1], dtype=torch.float32)
+        labels = torch.tensor([1.0, 0.0, 0.0, 0.0, 1.0])
         predictions = torch.tensor([0.6, 0.4, 0.55, 0.45, 0.5])
         indicator = torch.tensor([0, 0, 1, 1, 1])
         gauc_metric = Gauc()
